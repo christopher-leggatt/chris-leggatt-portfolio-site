513c57a03ccaa732baf48ed902c6de326da0f18f
Chris Leggatt <twoshow1978@gmail.com>
2023-11-12 14:56:16 -0500

    added Toolbox page content

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -11,9 +11,119 @@ export const timelineData = [
 ];
 
 export const softwareData: SoftwareItem[] = [
-  { url: "", title: "", types: ['', ''], description: "" },
+  {
+    url: "https://code.visualstudio.com/",
+    title: "VSCode",
+    types: ["Code Editor", "Development Tool"],
+    description:
+      "Visual Studio Code is a streamlined code editor with support for development operations like debugging, task running, and version control. It aims to provide the tools a developer needs for a quick code-build-debug cycle.",
+  },
+  {
+    url: "https://www.postman.com/",
+    title: "Postman",
+    types: ["API Testing", "Development Tool"],
+    description:
+      "Postman is an API platform for building and using APIs. Postman simplifies each step of the API lifecycle and streamlines collaboration so you can create better APIs—faster.",
+  },
+  {
+    url: "https://slack.com/",
+    title: "Slack",
+    types: ["Communication", "Team Collaboration"],
+    description:
+      "Slack is a new way to communicate with your team. It’s faster, better organized, and more secure than email.",
+  },
+  {
+    url: "https://www.mysql.com/products/workbench/",
+    title: "MySQL Workbench",
+    types: ["Database", "Development Tool"],
+    description:
+      "MySQL Workbench is a unified visual tool for database architects, developers, and DBAs. MySQL Workbench provides data modeling, SQL development, and comprehensive administration tools for server configuration, user administration, and much more.",
+  },
 ];
 
 export const techStackData: TechStackItem[] = [
-  { url: "", title: "", types: ['', ''], description: "" },
+  {
+    url: "https://sass-lang.com/",
+    title: "SASS",
+    types: ["CSS Preprocessor", "Style"],
+    description:
+      "SASS is a powerful CSS preprocessor that helps to keep stylesheets well-organized and makes it easier to share design within and across projects.",
+  },
+  {
+    url: "https://reactjs.org/",
+    title: "React",
+    types: ["JavaScript Library", "UI"],
+    description:
+      "React is a JavaScript library for building user interfaces. It allows developers to create large web applications that can change data, without reloading the page.",
+  },
+  {
+    url: "https://redux.js.org/",
+    title: "Redux",
+    types: ["State Management", "JavaScript"],
+    description:
+      "Redux is a predictable state container for JavaScript apps. It helps you write applications that behave consistently and are easy to test.",
+  },
+  {
+    url: "https://www.javascript.com/",
+    title: "JavaScript (JS)",
+    types: ["Programming Language", "Web Development"],
+    description:
+      "JavaScript is a programming language that enables interactive web pages. It is a part of most web browsers and allows for client-side script to interact with the user.",
+  },
+  {
+    url: "https://nextjs.org/",
+    title: "Next.js",
+    types: ["JavaScript Framework", "React"],
+    description:
+      "Next.js is a React framework that enables functionality such as server-side rendering and generating static websites for React-based web applications.",
+  },
+  {
+    url: "https://mui.com/",
+    title: "Material UI",
+    types: ["UI Framework", "React"],
+    description:
+      "Material UI is a popular React UI framework that implements Google's Material Design. It provides a set of components that helps in building consistent UIs.",
+  },
+  {
+    url: "https://chakra-ui.com/",
+    title: "Chakra UI",
+    types: ["UI Framework", "React"],
+    description:
+      "Chakra UI is a simple, modular, and accessible component library that gives you all the building blocks you need to build your React applications.",
+  },
+  {
+    url: "https://www.mongodb.com/",
+    title: "MongoDB",
+    types: ["Database", "NoSQL"],
+    description:
+      "MongoDB is a document database, which means it stores data in JSON-like documents. It provides a rich set of features and is known for its scalability.",
+  },
+  {
+    url: "https://www.mysql.com/",
+    title: "MySQL",
+    types: ["Database", "SQL"],
+    description:
+      "MySQL is one of the world's most popular relational database management systems. It is reliable, scalable, and easy to use.",
+  },
+  {
+    url: "https://knexjs.org/",
+    title: "Knex",
+    types: ["SQL Query Builder", "JavaScript"],
+    description:
+      "Knex.js is a SQL query builder for JavaScript, designed to be flexible, portable, and fun to use. It supports traditional node callbacks, promises, and async/await.",
+  },
+  {
+    url: "https://nodejs.org/",
+    title: "Node.js",
+    types: ["JavaScript Runtime", "Server-side"],
+    description:
+      "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine. It allows developers to build scalable network applications in JavaScript.",
+  },
+  {
+    url: "https://expressjs.com/",
+    title: "Express.js",
+    types: ["Web Framework", "Node.js"],
+    description:
+      "Express.js is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.",
+  },
 ];
