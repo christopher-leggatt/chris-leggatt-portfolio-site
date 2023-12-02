d8a6ab0f7cac6ed5783681788a1016e5ccfcf3d1
Chris Leggatt <twoshow1978@gmail.com>
2023-12-02 17:53:26 -0500

    added styling/functionality/api to Toolbox page

--- src/app/toolbox/page.tsx
+++ src/app/toolbox/page.tsx
@@ -1,15 +1,13 @@
-import { softwareData, techStackData } from "@/app/data/data";
 import PopoverLink from "@/app/(components)/PopoverLink";
+import { getToolbox } from "@/lib/toolbox-db";
 import { ToolboxProps } from "../interfaces";
 
 export const metadata = {
   title: "Home",
 };
 
-const Toolbox: React.FC<ToolboxProps> = ({
-  software = softwareData,
-  techStack = techStackData,
-}) => {
+const Toolbox = async () => {
+  const { toolbox } = (await getToolbox()) as { toolbox: ToolboxProps };
   return (
     <>
       <h1>
@@ -24,7 +22,7 @@ const Toolbox: React.FC<ToolboxProps> = ({
         <div className="space-y-12">
           <h2>Software</h2>
           <div className="grid grid-cols-1 gap-12 md:grid-cols-4">
-            {software?.map((item, index) => (
+            {toolbox?.software?.map((item, index) => (
               <div key={`${item.url}${index}`}>
                 <h3 className="m-0 text-xl font-medium">{item.title}</h3>
                 <div className="block space-x-4">
@@ -47,9 +45,34 @@ const Toolbox: React.FC<ToolboxProps> = ({
           </div>
         </div>
         <div className="space-y-12">
-          <h2>This Site & Tech Stack</h2>
+          <h2>Tech Stack</h2>
           <div className="grid grid-cols-1 gap-12 md:grid-cols-4">
-            {techStack?.map((item, index) => (
+            {toolbox?.tech_stack?.map((item, index) => (
+              <div key={`${item.url}${index}`}>
+                <h3 className="m-0 text-xl font-medium">{item.title}</h3>
+                <div className="block space-x-4">
+                  {item.types.map((tag: string) => (
+                    <span key={tag} className="text-sm">
+                      #{tag}
+                    </span>
+                  ))}
+                </div>
+                <div className="mt-4">
+                  <p className="m-0 mb-3 text-base">{item.description}</p>
+                  {item.url && (
+                    <span className="text-base">
+                      <PopoverLink href={item.url}>Check it out</PopoverLink>
+                    </span>
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="space-y-12">
+          <h2>This Site</h2>
+          <div className="grid grid-cols-1 gap-12 md:grid-cols-4">
+            {toolbox?.site?.map((item, index) => (
               <div key={`${item.url}${index}`}>
                 <h3 className="m-0 text-xl font-medium">{item.title}</h3>
                 <div className="block space-x-4">
