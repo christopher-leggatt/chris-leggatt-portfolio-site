2936912d6f808c244627cca42dbdc2b042af3b4b
Chris Leggatt <twoshow1978@gmail.com>
2023-12-30 21:10:23 -0500

    another bugfix attempt; reconfigure github workflow

--- src/app/api/experience/route.ts
+++ src/app/api/experience/route.ts
@@ -3,8 +3,6 @@ import { getExperience } from "@/lib/experience-db";
 import { NextResponse, NextRequest } from "next/server";
 import { createErrorResponse } from "@/lib/utils";
 
-export const dynamic = "force-dynamic";
-
 export async function GET(req: NextRequest) {
   try {
     await connectDB();
